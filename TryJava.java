4a596d31ebabcaad988a69187900d47813ae4870
XJefferson <67351478+XJeffersonLakashaing@users.noreply.github.com>
2020-07-19 17:06:44 +0530

    Update TryJava.java

--- TryJava.java
+++ TryJava.java
@@ -1,1 +1,11 @@
-
+//<>
+public class TryJava{
+  public static void main(String args[]){
+    try{
+      int div = 10/0; 
+      System.out.println("10 divide by 0");
+    }catch(ArithematicException e){
+        System.out.println(e.getMessage()); 
+    }
+}
+}
